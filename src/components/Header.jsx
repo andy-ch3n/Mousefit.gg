26a29981e30dbfd298a22d85a5f40cd98642138a
Andrew Hang <Andrewhang94@gmail.com>
2021-11-09 15:02:35 -0800

    added navbar and removed default create react app html items

---
+++ src/components/Header.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { makeStyles } from '@mui/styles';
+import LoginIcon from '@mui/icons-material/Login';
+
+const useStyles = makeStyles({
+  logo: {
+    maxWidth: 75,
+    paddingRight: '10px',
+  },
+  customizeToolbar: {
+    minHeight: 85,
+  }
+});
+
+function Header() {
+
+  const classes = useStyles();
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" color="primary">
+        <Toolbar className={classes.customizeToolbar}>
+          {/* <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton> */}
+          <img src="MouseFitLogo.png" alt="logo" className={classes.logo} />
+          <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
+            Mousefit.gg
+          </Typography>
+          <Button color="inherit">Login</Button>
+          <LoginIcon />
+        </Toolbar>
+      </AppBar>
+    </Box>
+  )
+}
+
+export default Header
