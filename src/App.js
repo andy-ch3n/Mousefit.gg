b452bcb73ac9420d3b14e92e9dec457073528ae0
andy-ch3n <adch3n@gmail.com>
2021-11-11 15:12:25 -0800

    merging barebones result before pull

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ function App() {
         </div> */}
         <Container>
             <ResultsPage />
-            <Footer />
+            {/* <Footer /> */}
           </Container>
       </ThemeProvider>
     </>
--- src/components/ResultsPage.jsx
+++ src/components/ResultsPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux';
 import { Button, Grid, Container, Box, Stack } from '@mui/material';
+import Typography from '@mui/material/Typography'
 import { styled } from '@mui/material/styles'
 import Paper from '@mui/material/Paper';
 import './ResultsPageStyles.css'
@@ -15,6 +16,8 @@ import ShareButton from './ShareButton.jsx'
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
   padding: theme.spacing(1),
+  elevation: 5,
+  border: `1px solid black`,
   textAlign: 'center',
   color: theme.palette.text.primary,
 }));
@@ -67,16 +70,17 @@ function ResultsPage() {
       headers: {
         'Content-Type': 'application/json'
         // 'Content-Type': 'application/x-www-form-urlencoded',
-      }}).then((res) => res.json())
-        .then((result) => {
-           console.log('work', result)
-            setScraped(result);
-        },
+      }
+    }).then((res) => res.json())
+      .then((result) => {
+        console.log('work', result)
+        setScraped(result);
+      },
         (error) => {
-            console.log(error);
+          console.log(error);
         }
-    )
-}
+      )
+  }
 
 
 
@@ -92,58 +96,58 @@ function ResultsPage() {
   useEffect(() => {
     getMouseData()
     if (productData.length > 5) {
-    getMouseList()
+      getMouseList()
     }
   }, [])
 
 
   return (
     <div className="results-page">
-      <h1> Quiz Results Page </h1>
+      <Typography className="product-name" variant="h4">Your mouse is</Typography>
       <br />
       <br />
       <Grid container spacing={2} columns={16}>
         <Grid item xs={8}>
-      <Item> Your recommended mouse  </Item>
-      <h3 className="product-name"> {productData.productName} </h3>
-      <p className="product-name">
-      <StarRatings/>
-      </p>
-      <div className="product-name">
+          <h3 className="product-name"> {productData && productData.productName} - {productData && productData.price.$numberDouble} </h3>
+          <p className="product-name">
+            <StarRatings />
+            <ShareButton />
+          </p>
+          <div className="product-name">
             <ReactTinyLink
-              cardSize="large"
+              cardSize="small"
               showGraphic={true}
               maxLine={2}
               minLine={1}
-              width={"10vw"}
               className="product-name"
               description={null}
               url="https://www.amazon.com/ASUS-ROG-Chakram-Wireless-Optical/dp/B0859F3S39/ref=sr_1_3?crid=7M266WNLYNKC&keywords=asus+rog+chakram&qid=1636598115&qsid=145-9430918-4631208&sprefix=asus+rog+chakram%2Caps%2C205&sr=8-3&sres=B0859F3S39%2CB0937MPFN3%2CB09D6DMY6T%2CB079RYNX33%2CB07MWTHGWT%2CB08KFQVT5Y%2CB09726KT4R%2CB08J3WX8Q1%2CB07M65DNKH%2CB073JDLCKT%2CB06XT4B2B9%2CB08R14HS3R%2CB0874WT7ZT%2CB01EMP3XDY%2CB073JJXHSB%2CB07JH4RNYH&srpt=INPUT_MOUSE"
             />
-      </div>
-      <div className="product-name">
-      <ShareButton/>
-      </div>
+            <br />
+            <Button onClick={handleClick} color="secondary" variant="contained"> Click to buy on Amazon </Button>
+          </div>
         </Grid>
         <Grid item xs={8}>
-            <ReactPlayer
+          <ReactPlayer
             width="550px"
             url='https://www.youtube.com/watch?v=imE3jc3Iq3s'
-             />
-          <Item> Price of this mouse is <strong> {productData && productData.price.$numberDouble} </strong> </Item>
-          <Item> This mouse is available in <b>{productData.interface}</b> </Item>
-          <Item> This mouse is mainly for FPS gamers </Item>
-          <Item> Has a weight of {productData && productData.weight.$numberDouble}g </Item>
-          <Item> The mouse supports {productData.mouseGrip} grip </Item>
+          />
+          <br />
+          <Item> Related Mouses </Item>
         </Grid>
       </Grid>
-      <br />
       <Box sx={{ display: "flex", justifyContent: 'center' }}>
         <Button onClick={handleClick} color="secondary" variant="contained"> Click to buy on Amazon </Button>
-        <Button onClick={getMouseList}> test </Button>
+        <Button variant="contained" onClick={getMouseList}> Top 5 Mouses </Button>
       </Box>
-        <br />
+      <br />
     </div>
   );
 }
 export default ResultsPage;
+
+
+{/* <Item> This mouse is available in <b>{productData.interface}</b> </Item>
+<Item> This mouse is mainly for FPS gamers </Item>
+<Item> Has a weight of {productData && productData.weight.$numberDouble}g </Item>
+<Item> The mouse supports {productData.mouseGrip} grip </Item> */}
