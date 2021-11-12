25a8d1826df91172ca35a8109b8dda4f7dcc9e18
Andrew Hang <Andrewhang94@gmail.com>
2021-11-12 00:22:17 -0800

    fixed css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,29 +1,3 @@
-// import React from "react";
-// import Typography from "@mui/material/Typography";
-// import Container from "@mui/material/Container";
-
-
-
-// export default function Footer() {
-//   return (
-    // <Container
-    //         maxWidth="md"
-    //         component="footer"
-    //         sx={{
-    //           borderTop: (theme) => `1px solid ${theme.palette.divider}`,
-    //           mt: "auto",
-    //           py: [3],
-    //           minWidth: "100%",
-    //           backgroundColor: "#012a4a"
-
-    //         }}
-    //       >
-    //         <Copyright sx={{ mt: 5 }} />
-    //       </Container>
-//   );
-// }
-
-
 import React from 'react'
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
@@ -49,39 +23,11 @@ function Copyright() {
   );
 }
 
-
-const useStyles = makeStyles({
-  logo: {
-    maxWidth: 75,
-    paddingRight: '10px',
-  },
-  customizeToolbar: {
-    minHeight: 85,
-  }
-});
-
 function Footer() {
 
-
-  const classes = useStyles();
-
   return (
-    <Box sx={{ position: 'fixed', width: '100%', bottom: 0, left: 0 }}>
+    <Box sx={{ width: '100%', bottom: 0, left: 0 }}>
       <AppBar position="static" color="secondary" textAlign="center">
-          {/* <img src="MouseFitLogo.png" alt="logo" className={classes.logo} />
-          <div className="footerClass">
-          <Typography
-            variant="body2"
-            color="#FFFFFF"
-            align="center"
-            textAlign="center"
-            sx={{flexGrow: 1}}
-          >
-            {"LiquidHackathon II. Mousefit.gg - Copyright © "}
-            {new Date().getFullYear()}
-            {"."}
-          </Typography>
-          </div> */}
             <Container
             maxWidth="md"
             component="footer"
--- src/components/ResultsPage.jsx
+++ src/components/ResultsPage.jsx
@@ -1,86 +1,90 @@
-import React, { useState, useEffect } from 'react'
-import { useSelector } from 'react-redux';
-import { Button, Grid, Container, Box, Stack, Card, CardContent } from '@mui/material';
-import Typography from '@mui/material/Typography'
-import { styled } from '@mui/material/styles'
-import Paper from '@mui/material/Paper';
-import './ResultsPageStyles.css'
-import dummydata1 from './dummydata.js'
-import { ReactTinyLink } from 'react-tiny-link';
-import ReactPlayer from 'react-player'
-import StarRatings from './StarRatings.jsx'
-import ShareButton from './ShareButton.jsx'
-import { setFinalMouse, getFinalMouse } from './redux/state/finalMouseSlice.js'
-import PanToolOutlinedIcon from '@mui/icons-material/PanToolOutlined';
-import FitnessCenterOutlinedIcon from '@mui/icons-material/FitnessCenterOutlined';
-import PermDataSettingIcon from '@mui/icons-material/PermDataSetting';
-import AspectRatioIcon from '@mui/icons-material/AspectRatio';
-import ControlPointDuplicateIcon from '@mui/icons-material/ControlPointDuplicate';
-import Footer from './Footer.jsx'
-
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import {
+  Button,
+  Grid,
+  Container,
+  Box,
+  Stack,
+  Card,
+  CardContent,
+} from "@mui/material";
+import Typography from "@mui/material/Typography";
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+import "./ResultsPageStyles.css";
+import dummydata1 from "./dummydata.js";
+import { ReactTinyLink } from "react-tiny-link";
+import ReactPlayer from "react-player";
+import StarRatings from "./StarRatings.jsx";
+import ShareButton from "./ShareButton.jsx";
+import Header from "./Header.jsx";
+import { setFinalMouse, getFinalMouse } from "./redux/state/finalMouseSlice.js";
+import PanToolOutlinedIcon from "@mui/icons-material/PanToolOutlined";
+import FitnessCenterOutlinedIcon from "@mui/icons-material/FitnessCenterOutlined";
+import PermDataSettingIcon from "@mui/icons-material/PermDataSetting";
+import AspectRatioIcon from "@mui/icons-material/AspectRatio";
+import ControlPointDuplicateIcon from "@mui/icons-material/ControlPointDuplicate";
+import Footer from "./Footer.jsx";
 
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
   padding: theme.spacing(1),
   elevation: 5,
   border: `1px solid navy`,
-  textAlign: 'center',
+  textAlign: "center",
   padding: "10px",
   color: theme.palette.text.primary,
 }));
 
-
 function ResultsPage() {
-  const [dummyData, setDummyData] = useState([])
-  const [productData, setProductData] = useState('')
+  const [dummyData, setDummyData] = useState([]);
+  const [productData, setProductData] = useState("");
 
-  const [productURL, setProductURL] = useState('')
-  const [mouseData, setMouseData] = useState('')
-  const [scraped, setScraped] = useState('')
+  const [productURL, setProductURL] = useState("");
+  const [mouseData, setMouseData] = useState("");
+  const [scraped, setScraped] = useState("");
 
-  const finalResult = useSelector(getFinalMouse)
+  const finalResult = useSelector(getFinalMouse);
 
   const getMouseList = () => {
     fetch("http://localhost:1337/api/getScrapedData/", {
-      method: 'GET', // *GET, POST, PUT, DELETE, etc.
-      mode: 'cors', // no-cors, *cors, same-origin
+      method: "GET", // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, *cors, same-origin
       body: JSON.stringify({
-        url: productData.amazonLink
+        url: productData.amazonLink,
       }),
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
         // 'Content-Type': 'application/x-www-form-urlencoded',
-      }
-    }).then((res) => res.json())
-      .then((result) => {
-        console.log('work', result)
-        setScraped(result);
       },
+    })
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          console.log("work", result);
+          setScraped(result);
+        },
         (error) => {
           console.log(error);
         }
-      )
-  }
-
-
+      );
+  };
 
   function getMouseData() {
-    setProductData(dummydata1.data[0])
+    setProductData(dummydata1.data[0]);
   }
-
 
   function handleClick() {
     window.location.assign(`${productData.amazonLink}`);
   }
 
   useEffect(() => {
-    getMouseData()
+    getMouseData();
     if (productData.length > 5) {
-      getMouseList()
+      getMouseList();
     }
-  }, [])
-
-
+  }, []);
 
   // function gripLogic() {
   //   if (productData.mouseGrip.includes())
@@ -115,7 +119,7 @@ function ResultsPage() {
         </Typography>
         <Typography variant="body2">
           <br />
-          {'It is most popularly used in FPS games.'}
+          {"It is most popularly used in FPS games."}
         </Typography>
       </CardContent>
     </React.Fragment>
@@ -148,14 +152,13 @@ function ResultsPage() {
           Made by brandNameSliced
         </Typography>
         <Typography variant="body2">
-          {'People rated this mouse averageMouseRating out of 5 on Amazon'}
+          {"People rated this mouse averageMouseRating out of 5 on Amazon"}
           <br />
           <br />
         </Typography>
       </CardContent>
     </React.Fragment>
   );
-
 
   const related = (
     <React.Fragment>
@@ -170,7 +173,6 @@ function ResultsPage() {
         <Typography variant="body2">
           <br />
           <br />
-
           People rated this mouse averageMouseRating out of 5 on Amazon
           <br />
         </Typography>
@@ -178,82 +180,161 @@ function ResultsPage() {
     </React.Fragment>
   );
 
-
-
-
   return (
     <>
-    <Container>
-      <div className="results-page">
-        <article>
-          <h1>YOUR PERFECT MOUSE IS </h1>
-        </article>
-        <Typography className="product-name" variant="h4">Your mouse is</Typography>
-        <br />
-        <br />
-        <div>
-        </div>
-        <Grid container spacing={2} columns={16}>
-          <Grid item xs={8}>
-            <Item className="name">
-              <h3 className="product-name"> {productData && productData.productName} - {productData && productData.price.$numberDouble} </h3>
-            </Item>
-            <div className="product-name">
+      <Container
+        sx={{
+          flexGrow: 1,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          padding: "30px",
+        }}
+      >
+        <div className="results-page">
+          <article>
+            <h1>YOUR PERFECT MOUSE IS </h1>
+          </article>
+          <br />
+          <br />
+          <div></div>
+          <Grid
+            container
+            spacing={2}
+            columns={16}
+            sx={{ marginLeft: "0", width: "100%" }}
+          >
+            <Grid item xs={8}>
+              <Item className="name">
+                <h3 className="product-name">
+                  {" "}
+                  {productData && productData.productName} -{" "}
+                  {productData && productData.price.$numberDouble}{" "}
+                </h3>
+              </Item>
+              <div className="product-name">
+                <br />
+              </div>
+              <p className="product-name">
+                <StarRatings />
+                <ShareButton />
+              </p>
               <br />
-            </div>
-            <p className="product-name">
-              <StarRatings />
-              <ShareButton />
-            </p>
-            <br />
-            <div className="product-name">
-              <Button className="product-name" onClick={handleClick} color="secondary" variant="contained"> Click to buy on Amazon </Button>
-            </div>
+              <div className="product-name">
+                <Button
+                  className="product-name"
+                  onClick={handleClick}
+                  color="secondary"
+                  variant="contained"
+                >
+                  {" "}
+                  Click to buy on Amazon{" "}
+                </Button>
+              </div>
+            </Grid>
+            <Grid item xs={8}>
+              <ReactPlayer
+                width="525px"
+                url="https://www.youtube.com/watch?v=imE3jc3Iq3s"
+              />
+              <br />
+              <Item className="related1">
+                <div className="relatedMouse">
+                  {" "}
+                  <ControlPointDuplicateIcon /> <strong>
+                    Related Mouses
+                  </strong>{" "}
+                </div>
+                <p> mouse 1 </p>
+                <p> mouse 2 </p>
+              </Item>
+            </Grid>
           </Grid>
-          <Grid item xs={8}>
-            <ReactPlayer
-              width="525px"
-              url='https://www.youtube.com/watch?v=imE3jc3Iq3s'
-            />
-            <br />
-            <Item className="related1">
-              <div className="relatedMouse"> <ControlPointDuplicateIcon /> <strong>Related Mouses</strong> </div>
-              <p> mouse 1 </p>
-              <p> mouse 2 </p>
-            </Item>
+          <br />
+          <Box sx={{ display: "flex", justifyContent: "center" }}></Box>
+          <br />
+          <Box
+            sx={{
+              height: "16px",
+              borderTop: "1px solid black",
+              marginLeft: "16px",
+              marginRight: "16px",
+            }}
+          />
+          <Grid
+            container
+            spacing={{ xs: 2, md: 3 }}
+            columns={{ xs: 4, sm: 8, md: 12 }}
+          >
+            <Grid item xs={2} sm={4} md={4}>
+              <Item
+                sx={{
+                  border: 1,
+                  marginLeft: "15px",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+                className="grip"
+              >
+                <PanToolOutlinedIcon />
+                <Box sx={{ marginLeft: "8px" }}>
+                  <strong>Mouse Grip</strong>
+                </Box>
+              </Item>
+              <Card sx={{ border: 1, marginLeft: "15px" }} variant="outlined">
+                {grip}
+              </Card>
+            </Grid>
+            <Grid item xs={2} sm={4} md={4}>
+              <Item
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <FitnessCenterOutlinedIcon />
+                <Box sx={{ marginLeft: "8px" }}>
+                  <strong>Weight and Size</strong>
+                </Box>
+              </Item>
+              <Card sx={{ border: 1 }} variant="outlined">
+                {weight}
+              </Card>
+            </Grid>
+            <Grid item xs={2} sm={4} md={4}>
+              <Item
+                sx={{
+                  border: 1,
+                  marginRight: "15px",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                {" "}
+                <AspectRatioIcon />
+                <Box sx={{ marginLeft: "8px" }}>
+                  <strong>Misc/Additional Info</strong>
+                </Box>
+              </Item>
+              <Card sx={{ border: 1, marginRight: "15px" }} variant="outlined">
+                {misc}
+              </Card>
+            </Grid>
           </Grid>
-        </Grid>
-        <br />
-        <Box sx={{ display: "flex", justifyContent: 'center' }}>
-        </Box>
-        <br />
-        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-          <Grid item xs={2} sm={4} md={4}>
-            <Item sx={{ border: 1, marginLeft: "15px" }} className="grip">
-              <PanToolOutlinedIcon /> <strong>Mouse Grip</strong> </Item>
-            <Card sx={{ border: 1, marginLeft: "15px" }} variant="outlined">{grip}</Card>
-          </Grid>
-          <Grid item xs={2} sm={4} md={4}>
-            <Item>
-              <FitnessCenterOutlinedIcon /> <strong>Weight and Size </strong> </Item>
-            <Card sx={{ border: 1 }} variant="outlined">{weight}</Card>
-          </Grid>
-          <Grid item xs={2} sm={4} md={4}>
-            <Item sx={{ border: 1, marginRight: "15px" }}> <AspectRatioIcon />  <strong>Misc/Additional Info</strong> </Item>
-            <Card sx={{ border: 1, marginRight: "15px" }} variant="outlined">{misc}</Card>
-          </Grid>
-        </Grid>
-        <br />
-      </div>
-    </Container>
-    <Footer/>
+          <br />
+        </div>
+      </Container>
     </>
   );
 }
 export default ResultsPage;
 
-
-{/* <Item> This mouse is available in <b>{productData.interface}</b> </Item>
+{
+  /* <Item> This mouse is available in <b>{productData.interface}</b> </Item>
 <Item> This mouse is mainly for FPS gamers </Item>
 <Item> Has a weight of {productData && productData.weight.$numberDouble}g </Item>
-<Item> The mouse supports {productData.mouseGrip} grip </Item> */}
+<Item> The mouse supports {productData.mouseGrip} grip </Item> */
+}
