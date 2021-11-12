69c6bcc5401c1b7abeb5e0280365883f96311792
Andrew Hang <Andrewhang94@gmail.com>
2021-11-12 15:58:00 -0800

    removed cors

--- server/index.js
+++ server/index.js
@@ -12,27 +12,6 @@ server.use(cors());
 server.use(express.json());
 server.use("/api", router);
 
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://618ef857409fdb0007384d52--brave-raman-0b2501.netlify.app/",
-];
-server.use(
-  cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin
-      // (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        let msg =
-          "The CORS policy for this site does not " +
-          "allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
-
 const PORT = process.env.SERVER_PORT;
 
 server.listen(PORT, () => {
