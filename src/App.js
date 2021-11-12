71c1a5a7b13951073ac360dc191c5550422b8afb
andy-ch3n <adch3n@gmail.com>
2021-11-12 12:45:23 -0800

    scroll broken on my end?

--- src/App.js
+++ src/App.js
@@ -1,99 +1,58 @@
-import React, { useEffect } from "react";
-import { useSelector } from "react-redux";
-import { getClicked } from "./components/redux/state/quizButtonClickedSlice.js";
-import { getIsQuizDone } from "./components/redux/state/isQuizDoneSlice.js";
-import { getScrapedYoutube } from "./components/redux/state/scrapedYoutubeSlice.js";
-import { getScrapedData } from "./components/redux/state/scrapedDataSlice.js";
-import { ThemeProvider } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import Theme from "./Theme/ThemeFile.js";
-import Header from "./components/Header.jsx";
-import Mouse from "./components/Mouse.jsx";
-import QuizButton from "./components/QuizButton.jsx";
-import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
-import Footer from "./components/Footer.jsx";
-import ResultsPage from "./components/ResultsPage.jsx";
-import LoadingPage from "./components/LoadingPage.jsx";
+import React from "react"
+import { useSelector } from 'react-redux'
+import { getClicked } from './components/redux/state/quizButtonClickedSlice.js'
+import { ThemeProvider } from "@mui/material/styles"
+import Box from '@mui/material/Box'
+import Theme from "./Theme/ThemeFile.js"
+import Header from "./components/Header.jsx"
+import Mouse from "./components/Mouse.jsx"
+import QuizButton from './components/QuizButton.jsx'
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Footer from './components/Footer.jsx'
+import ResultsPage from './components/ResultsPage.jsx'
 
 function App() {
+  // const quizButtonClicked = useSelector(getQuizButtonClicked)
   const quizClicked = useSelector(getClicked);
-  const scrapedYoutube = useSelector(getScrapedYoutube);
-  const scrapedData = useSelector(getScrapedData);
-  const isQuizDone = useSelector(getIsQuizDone);
 
-  useEffect(() => {}, []);
-
-  const landingPage = () => {
-    if (!quizClicked.quizbuttonclicked) {
-      return <Mouse />;
-    }
-    return <></>;
-  };
-
-  const QuizDonePage = () => {
-    if (scrapedYoutube.isLoading && scrapedData.isLoading) {
-      return <LoadingPage />;
-    } else {
-      return <ResultsPage />;
-    }
-  };
-
-  const QuizDonePageTwo = () => {
-    if (!isQuizDone.done) {
-      return <QuizButton />;
-    } else if (
-      isQuizDone.done &&
-      scrapedYoutube.isLoading &&
-      scrapedData.isLoading
-    ) {
-      return <LoadingPage />;
-    } else {
-      return <ResultsPage />;
-    }
-  };
+  // const landingPage = () => {
+  //   if(!quizClicked.quizbuttonclicked) {
+  //     return (
+  //         // <Mouse />
+  //         <></>
+  //     )
+  //   }
+  //   return (<></>)
+  // }
 
   return (
     <>
       <ThemeProvider theme={Theme}>
-        <div className="stupid-star-animation">
-          <div id="stars"></div>
-          <div id="stars2"></div>
-          <div id="stars3"></div>
-        </div>
-        <div className="body-container-wrapper-thing">
-          <Header />
-          <div
-            style={{
-              width: "100%",
-              height:
-                !quizClicked.quizbuttonclicked ||
-                (scrapedYoutube.isLoading && scrapedData.isLoading)
-                  ? "100%"
-                  : null,
-              display: "flex",
-              alignItems: !quizClicked.quizbuttonclicked ? null : "center",
-              justifyContent: !quizClicked.quizbuttonclicked ? null : "center",
-              flexDirection: "column",
-            }}
+        <div id='stars'></div>
+        <div id='stars2'></div>
+        <div id='stars3'></div>
+        {/* <Header/>
+        <div
+        style={{
+          width: '100vw',
+          height: '100vh',
+          display: 'flex',
+          alignItems: !quizClicked.quizbuttonclicked ? null : 'center',
+          justifyContent: !quizClicked.quizbuttonclicked ? null : 'center',
+          flexDirection: 'column',
+        }}
+        >
+          {/* {landingPage()} }
+        <Box
+          position= {!quizClicked.quizbuttonclicked ? 'absolute' : 'static' }
+          top= {!quizClicked.quizbuttonclicked ? '75%' : null}
+          left= {!quizClicked.quizbuttonclicked ? '44%' : null}
           >
-            {landingPage()}
-            <Box
-              position={!quizClicked.quizbuttonclicked ? "absolute" : "static"}
-              bottom={!quizClicked.quizbuttonclicked ? "35%" : null}
-              left={!quizClicked.quizbuttonclicked ? "50%" : null}
-            >
-              {scrapedYoutube.isLoading && scrapedData.isLoading ? (
-                <QuizButton />
-              ) : (
-                <ResultsPage />
-              )}
-              {/* {QuizDonePageTwo()} */}
-            </Box>
-            {/* <LoadingPage /> */}
-          </div>
-          <Footer />
-        </div>
+           <QuizButton />
+        </Box>
+        </div> */}
+           <ResultsPage />
       </ThemeProvider>
     </>
   );
--- src/components/ResultsPage.jsx
+++ src/components/ResultsPage.jsx
@@ -5,30 +5,25 @@ import {
   Grid,
   Container,
   Box,
-  Stack,
   Card,
   CardContent,
   styled,
+  Paper,
   Typography
 } from "@mui/material";
-import Paper from "@mui/material/Paper";
 import "./ResultsPageStyles.css";
 import dummydata1 from "./dummydata.js";
-import { ReactTinyLink } from "react-tiny-link";
 import ReactPlayer from "react-player";
 import StarRatings from "./StarRatings.jsx";
 import ShareButton from "./ShareButton.jsx";
-import Header from "./Header.jsx";
-import { setFinalMouse, getFinalMouse } from "./redux/state/finalMouseSlice.js";
+import { getFinalMouse } from "./redux/state/finalMouseSlice.js";
 import PanToolOutlinedIcon from "@mui/icons-material/PanToolOutlined";
 import FitnessCenterOutlinedIcon from "@mui/icons-material/FitnessCenterOutlined";
-import PermDataSettingIcon from "@mui/icons-material/PermDataSetting";
 import AspectRatioIcon from "@mui/icons-material/AspectRatio";
 import ControlPointDuplicateIcon from "@mui/icons-material/ControlPointDuplicate";
-import Footer from "./Footer.jsx";
-import { setScrapedData, getScrapedData } from './redux/state/scrapedDataSlice.js';
-import { setRelatedMouse, getRelatedMouse } from './redux/state/relatedMouseSlice.js';
-import { setScrapedYoutube, getScrapedYoutube } from './redux/state/scrapedYoutubeSlice.js';
+import { getScrapedData } from './redux/state/scrapedDataSlice.js';
+import { getRelatedMouse } from './redux/state/relatedMouseSlice.js';
+import { getScrapedYoutube } from './redux/state/scrapedYoutubeSlice.js';
 
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
@@ -43,18 +38,40 @@ const Item = styled(Paper)(({ theme }) => ({
 function ResultsPage() {
   const [dummyData, setDummyData] = useState([]);
   const [productData, setProductData] = useState("");
-
-  const [productURL, setProductURL] = useState("");
-  const [mouseData, setMouseData] = useState("");
-  const [scraped, setScraped] = useState("");
+  const [weightMeasure, setWeightMeasure] = useState("");
+  const [sizeMeasure, setSizeMeasure] = useState("");
 
   const finalResult = useSelector(getFinalMouse);
   const scrapedData = useSelector(getScrapedData);
-	const relatedMice = useSelector(getRelatedMouse);
-	const scrapedYoutube = useSelector(getScrapedYoutube);
+  const relatedMice = useSelector(getRelatedMouse);
+  const scrapedYoutube = useSelector(getScrapedYoutube);
 
   function getMouseData() {
     setProductData(dummydata1.data[0]);
+  }
+
+
+  function getWeightMeasure() {
+    if (productData && productData.weight.$numberDouble < 60) {
+      setWeightMeasure('very light (<60g)')
+    } else if (productData && (productData.weight.$numberDouble >= 60 && productData.weight.$numberDouble < 75)) {
+      setWeightMeasure('light (60-75g)')
+    } else if (productData && (productData.weight.$numberDouble >= 75 && productData.weight.$numberDouble < 100)) {
+      setWeightMeasure('medium, (75-100g)')
+    } else {
+      setWeightMeasure('heavy, (100g+)')
+    }
+  }
+
+  function getSizeMeasure() {
+    if (productData && productData.mouseSize.$numberDouble < 310000) {
+      setSizeMeasure('small (<310,000mm^3)')
+    } else if (productData && (productData.mouseSize.$numberDouble >= 75 && productData.mouseSize.$numberDouble < 100)) {
+      setSizeMeasure('medium, (310,000-370,000mm^3)')
+    } else {
+      setSizeMeasure('large, (350,000mm^3+)')
+    }
+
   }
 
   function handleClick() {
@@ -62,7 +79,10 @@ function ResultsPage() {
   }
 
   useEffect(() => {
+
     getMouseData();
+    getWeightMeasure()
+    getSizeMeasure()
   }, []);
 
 
@@ -70,15 +90,16 @@ function ResultsPage() {
     <React.Fragment>
       <CardContent>
         <Typography variant="h6" component="div">
-          Mouse made for {productData && productData.mouseGrip.replace(/\[|\]/g,'')} users
+          Mouse made for {productData && productData.mouseGrip.replace(/\[|\]/g, '')} users
         </Typography>
         <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          Palm grip is the most widely used mouse grip.
+          {productData && productData.mouseGrip.includes('palm') ? <p> Palm grip is the most widely used mouse grip.</p> : <p> Claw is the second most widely used mouse grip. </p>}
         </Typography>
+        <br />
         <Typography variant="body2">
           <br />
           <div className="ital">
-          {"It is most popularly used in FPS games."}
+            {"ProTip: FPS gamers most commonly use the palm grip. MOBA gamers are most commonly claw grip."}
           </div>
         </Typography>
       </CardContent>
@@ -89,16 +110,23 @@ function ResultsPage() {
     <React.Fragment>
       <CardContent>
         <Typography variant="h6" component="div">
-          Considered a <strong>Heavy</strong> (>100g) mouse.
+          Considered a <strong>{weightMeasure}</strong> mouse.
         </Typography>
+        <br />
         <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          Classified as a <strong>Large size (>222222mm) </strong>
+          Mouse is <strong>{sizeMeasure}</strong>
+          <br />
+          <br />
+          <div>
+            Calculated using <strong> Length x Width x Height</strong>
+          </div>
         </Typography>
         <Typography variant="body2">
           <br />
           <div className="ital">
-          {`The heavier the mouse, the higher one's in-game sensitivity should be`}
+            {`ProTip: The heavier the mouse, the higher one's in-game sensitivity should generally be.`}
           </div>
+          <br />
         </Typography>
       </CardContent>
     </React.Fragment>
@@ -108,36 +136,20 @@ function ResultsPage() {
     <React.Fragment>
       <CardContent>
         <Typography variant="h6" component="div">
-          Available in {productData && productData.interface.replace(/\[|\]/g,'')}.
+          Available in {productData && productData.interface.replace(/\[|\]/g, '')}.
         </Typography>
         <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          Made by {productData && productData.productName.split(" ")[0]}
-        </Typography>
-        <Typography variant="body2">
-        <div className="ital">
-          {"People rated this mouse averageMouseRating out of 5 on Amazon"}
-          </div>
           <br />
-        </Typography>
-      </CardContent>
-    </React.Fragment>
-  );
-
-  const related = (
-    <React.Fragment>
-      <CardContent>
-        <Typography variant="h6" component="div">
-          Available in {productData.interface}.
+          Made by <strong>{productData && productData.productName.split(" ")[0]}</strong>
         </Typography>
         <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          Made by brandNameSliced
+          {productData && productData.price.$numberDouble > 100
+            ? <p>This is considered an <strong>expensive ($100+)</strong> price for a mouse. </p>
+            : <p>This is considered an <strong>average</strong> price for a mouse.</p>}
         </Typography>
-        <br />
         <Typography variant="body2">
-          <br />
-          <br />
           <div className="ital">
-          People rated this mouse averageMouseRating out of 5 on Amazon
+            {`ProTip: People rated this mouse ${scrapedData.data.rating} out of 5 on Amazon`}
           </div>
           <br />
         </Typography>
@@ -171,7 +183,7 @@ function ResultsPage() {
           >
             <Grid item xs={8}>
               <Item className="name">
-                <h2 className="product-header">
+                <h2 className="gradient-name">
                   {" "}
                   {productData && productData.productName} - {" "}
                   {productData && productData.price.$numberDouble}{" "}
@@ -192,21 +204,21 @@ function ResultsPage() {
                 <Box
                   component="img"
                   alt={`Everything you need to know about ${productData.productName}`}
-                  // src={scrapedData.data.image}
                   sx={{
                     height: "100%",
                     width: "150px",
                     opacity: "0.9"
                   }}
                   src={"https://m.media-amazon.com/images/I/5152oFkzhLL._AC_SY450_.jpg"}
+                // instead src={scrapedData.data.image}
                 />
               </Box>
-              <p className="product-name">
+              <div className="social-stars">
                 <div className="product-stars">
-                <StarRatings />
+                  <StarRatings />
                 </div>
                 <ShareButton />
-              </p>
+              </div>
               <br />
               <div className="product-name">
                 <Button
@@ -224,7 +236,7 @@ function ResultsPage() {
               <Item className="reactPlayer">
                 <ReactPlayer
                   width="525px"
-                  // url={scrapedYoutube.data[0].link}
+                  // instead url={scrapedYoutube.data[0].link}
                   url={"https://www.youtube.com/watch?v=FGwzq4UMnVs&list=RDFGwzq4UMnVs&start_radio=1&ab_channel=Ti%C3%ABsto"}
                 />
               </Item>
@@ -235,7 +247,7 @@ function ResultsPage() {
                   <ControlPointDuplicateIcon /> <strong>
                     Related Mouses
                   </strong>{" "}
-                {(relatedMice.relatedmouse.length >= 1 && relatedMice.relatedmouse.length) < 3 ? <p>{relatedMice.relatedmouse.map(result => result.productName)}</p> : <p> No related products to display</p>}
+                  {(relatedMice.relatedmouse.length >= 1 && relatedMice.relatedmouse.length) < 3 ? <p>{relatedMice.relatedmouse.map(result => result.productName)}</p> : <p> No related products to display</p>}
                 </div>
               </Item>
             </Grid>
@@ -274,7 +286,7 @@ function ResultsPage() {
                 className="grip"
               >
                 <PanToolOutlinedIcon />
-                <Box sx={{ marginLeft: "8px" }}>
+                <Box sx={{ marginLeft: "8px", textDecoration: "underline" }}>
                   <strong>Mouse Grip</strong>
                 </Box>
               </Item>
@@ -296,7 +308,7 @@ function ResultsPage() {
                 }}
               >
                 <FitnessCenterOutlinedIcon />
-                <Box sx={{ marginLeft: "8px" }}>
+                <Box sx={{ marginLeft: "8px", textDecoration: "underline" }}>
                   <strong>Weight and Size</strong>
                 </Box>
               </Item>
@@ -322,7 +334,7 @@ function ResultsPage() {
               >
                 {" "}
                 <AspectRatioIcon />
-                <Box sx={{ marginLeft: "8px" }}>
+                <Box sx={{ marginLeft: "8px", textDecoration: "underline" }}>
                   <strong>Misc & Additional Info</strong>
                 </Box>
               </Item>
