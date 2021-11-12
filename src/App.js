812f48b71ec2523d993e2895dc97b64a8706ff8c
Andrew Hang <Andrewhang94@gmail.com>
2021-11-12 03:07:18 -0800

    added loading page

--- src/App.js
+++ src/App.js
@@ -1,60 +1,92 @@
-import React from "react"
-import { useSelector } from 'react-redux'
-import { getClicked } from './components/redux/state/quizButtonClickedSlice.js'
-import { getIsQuizDone } from './components/redux/state/isQuizDoneSlice.js';
-import { ThemeProvider } from "@mui/material/styles"
-import Box from '@mui/material/Box'
-import Theme from "./Theme/ThemeFile.js"
-import Header from "./components/Header.jsx"
-import Mouse from "./components/Mouse.jsx"
-import QuizButton from './components/QuizButton.jsx'
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import Footer from './components/Footer.jsx'
-import ResultsPage from './components/ResultsPage.jsx'
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { getClicked } from "./components/redux/state/quizButtonClickedSlice.js";
+import { getIsQuizDone } from "./components/redux/state/isQuizDoneSlice.js";
+import { getScrapedYoutube } from "./components/redux/state/scrapedYoutubeSlice.js";
+import { getScrapedData } from "./components/redux/state/scrapedDataSlice.js";
+import { ThemeProvider } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Theme from "./Theme/ThemeFile.js";
+import Header from "./components/Header.jsx";
+import Mouse from "./components/Mouse.jsx";
+import QuizButton from "./components/QuizButton.jsx";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Footer from "./components/Footer.jsx";
+import ResultsPage from "./components/ResultsPage.jsx";
+import LoadingPage from "./components/LoadingPage.jsx";
 
 function App() {
   const quizClicked = useSelector(getClicked);
+  const scrapedYoutube = useSelector(getScrapedYoutube);
+  const scrapedData = useSelector(getScrapedData);
   const isQuizDone = useSelector(getIsQuizDone);
 
+  useEffect(() => {}, []);
+
   const landingPage = () => {
-    if(!quizClicked.quizbuttonclicked) {
-      return (
-          // <Mouse />
-          <></>
-      )
+    if (!quizClicked.quizbuttonclicked) {
+      return <Mouse />;
     }
-    return (<></>)
-  }
+    return <></>;
+  };
+
+  const QuizDonePage = () => {
+    if (scrapedYoutube.isLoading && scrapedData.isLoading) {
+      return <LoadingPage />;
+    } else {
+      return <ResultsPage />;
+    }
+  };
+
+  const QuizDonePageTwo = () => {
+    if (!isQuizDone.done) {
+      return <QuizButton />;
+    } else if (
+      isQuizDone.done &&
+      scrapedYoutube.isLoading &&
+      scrapedData.isLoading
+    ) {
+      return <LoadingPage />;
+    } else {
+      return <ResultsPage />;
+    }
+  };
 
   return (
     <>
       <ThemeProvider theme={Theme}>
-        <div className='stupid-star-animation'>
-          <div id='stars'></div>
-          <div id='stars2'></div>
-          <div id='stars3'></div>
+        <div className="stupid-star-animation">
+          <div id="stars"></div>
+          <div id="stars2"></div>
+          <div id="stars3"></div>
         </div>
-        <div className='body-container-wrapper-thing'>
-          <Header/>
+        <div className="body-container-wrapper-thing">
+          <Header />
           <div
             style={{
-              width: '100%',
-              height: '100%',
-              display: 'flex',
-              alignItems: !quizClicked.quizbuttonclicked ? null : 'center',
-              justifyContent: !quizClicked.quizbuttonclicked ? null : 'center',
-              flexDirection: 'column',
+              width: "100%",
+              height: "100%",
+              display: "flex",
+              alignItems: !quizClicked.quizbuttonclicked ? null : "center",
+              justifyContent: !quizClicked.quizbuttonclicked ? null : "center",
+              flexDirection: "column",
             }}
           >
             {landingPage()}
             <Box
-              position= {!quizClicked.quizbuttonclicked ? 'absolute' : 'static' }
-              bottom= {!quizClicked.quizbuttonclicked ? '35%' : null}
-              left= {!quizClicked.quizbuttonclicked ? '50%' : null}
+              position={!quizClicked.quizbuttonclicked ? "absolute" : "static"}
+              bottom={!quizClicked.quizbuttonclicked ? "35%" : null}
+              left={!quizClicked.quizbuttonclicked ? "50%" : null}
             >
-               {!isQuizDone.done ? <QuizButton /> : <ResultsPage /> }
+              {scrapedYoutube.isLoading && scrapedData.isLoading ? (
+                <QuizButton />
+              ) : (
+                <ResultsPage />
+              )}
+              {/* {QuizDonePageTwo()} */}
             </Box>
+            {/* <LoadingPage /> */}
           </div>
           <Footer />
         </div>
--- src/components/QuizPage.jsx
+++ src/components/QuizPage.jsx
@@ -1,168 +1,206 @@
-import React, { useState, useEffect } from 'react';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import { useSelector, useDispatch } from 'react-redux';
-import { setFinalMouse, getFinalMouse } from './redux/state/finalMouseSlice.js';
-import { setScrapedData, getScrapedData } from './redux/state/scrapedDataSlice.js';
-import { setIsQuizDone, getIsQuizDone } from './redux/state/isQuizDoneSlice.js';
-import { setRelatedMouse, getRelatedMouse } from './redux/state/relatedMouseSlice.js';
-import { setScrapedYoutube, getScrapedYoutube } from './redux/state/scrapedYoutubeSlice.js';
+import React, { useState, useEffect } from "react";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import LoadingPage from "./LoadingPage.jsx";
+import { useSelector, useDispatch } from "react-redux";
+import { setFinalMouse, getFinalMouse } from "./redux/state/finalMouseSlice.js";
+import {
+  setScrapedData,
+  getScrapedData,
+} from "./redux/state/scrapedDataSlice.js";
+import { setIsQuizDone, getIsQuizDone } from "./redux/state/isQuizDoneSlice.js";
+import {
+  setRelatedMouse,
+  getRelatedMouse,
+} from "./redux/state/relatedMouseSlice.js";
+import {
+  setScrapedYoutube,
+  getScrapedYoutube,
+} from "./redux/state/scrapedYoutubeSlice.js";
 
 export default function QuizPage() {
-	const questions = [
-		{
-			questionText: 'What your preferred mouse grip?',
-			type: 'mouseGrip',
-			answerOptions: [
-				{ answerText: 'Claw', value: 'Claw' },
-				{ answerText: 'Fingertip', value: 'Fingertip' },
-				{ answerText: 'Palm', value: 'Palm' },
-			],
-		},
-		{
-			questionText: 'Wired or Wireless?',
-			type: 'interface',
-			answerOptions: [
-				{ answerText: 'Wired', value: 'wired' },
-				{ answerText: 'Wireless', value: 'wireless' },
-			],
-		},
-		{
-			questionText: 'Light, Medium, or Heavy Weight?',
-			type: 'weight',
-			answerOptions: [
-				{ answerText: 'Light', value: [0, 80]},
-				{ answerText: 'Medium', value: [80, 120]},
-				{ answerText: 'Heavy', value: [120, 1000]},
-			],
-		},
-		{
-			questionText: 'Small, Medium, or Large Mouse Size?',
-			type: 'mouseSize',
-			answerOptions: [
-				{ answerText: 'Small', value: [0, 292800]},
-				{ answerText: 'Medium', value: [292800, 357000]},
-				{ answerText: 'Large', value: [357000, 100000000]},
-			],
-		},
-	];
+  const questions = [
+    {
+      questionText: "What your preferred mouse grip?",
+      type: "mouseGrip",
+      answerOptions: [
+        { answerText: "Claw", value: "Claw" },
+        { answerText: "Fingertip", value: "Fingertip" },
+        { answerText: "Palm", value: "Palm" },
+      ],
+    },
+    {
+      questionText: "Wired or Wireless?",
+      type: "interface",
+      answerOptions: [
+        { answerText: "Wired", value: "wired" },
+        { answerText: "Wireless", value: "wireless" },
+      ],
+    },
+    {
+      questionText: "Light, Medium, or Heavy Weight?",
+      type: "weight",
+      answerOptions: [
+        { answerText: "Light", value: [0, 80] },
+        { answerText: "Medium", value: [80, 120] },
+        { answerText: "Heavy", value: [120, 1000] },
+      ],
+    },
+    {
+      questionText: "Small, Medium, or Large Mouse Size?",
+      type: "mouseSize",
+      answerOptions: [
+        { answerText: "Small", value: [0, 292800] },
+        { answerText: "Medium", value: [292800, 357000] },
+        { answerText: "Large", value: [357000, 100000000] },
+      ],
+    },
+  ];
 
-	const [currentQuestion, setCurrentQuestion] = useState(0);
-	const [categoryType, setCategoryType] = useState('');
-	const [mouseList, setMouseList] = useState([]);
-	const isQuizDone = useSelector(getIsQuizDone);
-	const finalMouse = useSelector(getFinalMouse);
-	const scrapedData = useSelector(getScrapedData);
-	const relatedMice = useSelector(getRelatedMouse);
-	const scrapedYoutube = useSelector(getScrapedYoutube);
-	const dispatch = useDispatch();
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [categoryType, setCategoryType] = useState("");
+  const [mouseList, setMouseList] = useState([]);
+  const isQuizDone = useSelector(getIsQuizDone);
+  const finalMouse = useSelector(getFinalMouse);
+  const scrapedData = useSelector(getScrapedData);
+  const relatedMice = useSelector(getRelatedMouse);
+  const scrapedYoutube = useSelector(getScrapedYoutube);
+  const dispatch = useDispatch();
 
-	useEffect(() => {
-		getMouseList();
-	}, [])
+  useEffect(() => {
+    getMouseList();
+  }, []);
 
-	useEffect(() => {
-		setCategoryType(questions[currentQuestion].type);
-	}, [currentQuestion])
+  useEffect(() => {
+    setCategoryType(questions[currentQuestion].type);
+  }, [currentQuestion]);
 
-	useEffect(() => {
-		if (finalMouse.finalmouse.amazonLink !== undefined) {
-			getRelatedMice();
-			getYoutubeVid();
-			getScrapedMouse();
-		}
-	}, [finalMouse])
+  useEffect(() => {
+    if (finalMouse.finalmouse.amazonLink !== undefined) {
+      getRelatedMice();
+      getYoutubeVid();
+      getScrapedMouse();
+    }
+  }, [finalMouse]);
 
-	const getScrapedMouse = () => {
+  const getScrapedMouse = () => {
     const requestOptions = {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({ url: finalMouse.finalmouse.amazonLink })
-	};
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ url: finalMouse.finalmouse.amazonLink }),
+    };
 
-	fetch('http://localhost:1337/api/getScrapeData', requestOptions)
-			.then(response => response.json())
-			.then(data => dispatch(setScrapedData({ data: data })));
-	}
+    fetch("http://localhost:1337/api/getScrapeData", requestOptions)
+      .then((response) => response.json())
+      .then((data) =>
+        dispatch(setScrapedData({ data: data, isLoading: false }))
+      );
+  };
 
-	const getYoutubeVid = () => {
+  const getYoutubeVid = () => {
     const requestOptions = {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({ mouse: finalMouse.finalmouse.productName})
-	};
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ mouse: finalMouse.finalmouse.productName }),
+    };
 
-	fetch('http://localhost:1337/api/getLink', requestOptions)
-			.then(response => response.json())
-			.then(data => dispatch(setScrapedYoutube({ data: data })));
-	}
+    fetch("http://localhost:1337/api/getLink", requestOptions)
+      .then((response) => response.json())
+      .then((data) =>
+        dispatch(setScrapedYoutube({ data: data, isLoading: false }))
+      );
+  };
 
-	const getMouseList = () => {
-		fetch("http://localhost:1337/api/getAll")
-			.then((res) => res.json())
-			.then((result) => {
-				setMouseList(result);
-			},
-			(error) => {
-				console.log(error);
-			}
-		)
-	}
+  const getMouseList = () => {
+    fetch("http://localhost:1337/api/getAll")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setMouseList(result);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  };
 
-	const filterMouseList = (filterTerm, type) => {
-		let filteredList = [];
-		if (type === 'mouseGrip') {
-			filteredList = mouseList.filter(mouse => mouse[type][0].includes(filterTerm))
-		} else if (type === 'interface') {
-			filteredList = mouseList.filter(mouse => mouse[type][0].includes(filterTerm))
-		} else if (type === 'weight') {
-			filteredList = mouseList.filter(mouse => (mouse[type] >= filterTerm[0] && mouse[type] <= filterTerm[1]))
-		} else if (type === 'mouseSize') {
-			filteredList = mouseList.filter(mouse => (mouse[type] >= filterTerm[0] && mouse[type] <= filterTerm[1]))		}
-		setMouseList(filteredList);
-	}
+  const filterMouseList = (filterTerm, type) => {
+    let filteredList = [];
+    if (type === "mouseGrip") {
+      filteredList = mouseList.filter((mouse) =>
+        mouse[type][0].includes(filterTerm)
+      );
+    } else if (type === "interface") {
+      filteredList = mouseList.filter((mouse) =>
+        mouse[type][0].includes(filterTerm)
+      );
+    } else if (type === "weight") {
+      filteredList = mouseList.filter(
+        (mouse) => mouse[type] >= filterTerm[0] && mouse[type] <= filterTerm[1]
+      );
+    } else if (type === "mouseSize") {
+      filteredList = mouseList.filter(
+        (mouse) => mouse[type] >= filterTerm[0] && mouse[type] <= filterTerm[1]
+      );
+    }
+    setMouseList(filteredList);
+  };
 
-	const handleAnswerClick = (filterTerm, type) => {
-		filterMouseList(filterTerm, type)
+  const handleAnswerClick = (filterTerm, type) => {
+    filterMouseList(filterTerm, type);
 
-		const nextQuestion = currentQuestion + 1;
-		if (nextQuestion === questions.length) {
-			dispatch(setIsQuizDone({done: true}))
-		}
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion === questions.length) {
+      dispatch(setIsQuizDone({ done: true }));
+    }
 
-		if (nextQuestion < questions.length) {
-			setCurrentQuestion(nextQuestion);
-		} else {
-			const result = randomFinalMouse(mouseList);
-			dispatch(setFinalMouse({ finalmouse: result }))
-		}
-	}
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      const result = randomFinalMouse(mouseList);
+      dispatch(setFinalMouse({ finalmouse: result }));
+    }
+  };
 
-	function randomFinalMouse(arr) {
+  function randomFinalMouse(arr) {
     return arr[Math.floor(arr.length * Math.random())];
-	}
+  }
 
-	function getRelatedMice() {
-		const relatedList = mouseList.filter(mouse => (mouse.amazonLink !== finalMouse.finalmouse.amazonLink))
-		dispatch(setRelatedMouse({ relatedmouse: relatedList}))
-	}
+  function getRelatedMice() {
+    const relatedList = mouseList.filter(
+      (mouse) => mouse.amazonLink !== finalMouse.finalmouse.amazonLink
+    );
+    dispatch(setRelatedMouse({ relatedmouse: relatedList }));
+  }
 
-		return (
-				<Typography className='quiz-app' component="div">
-				<div style={{opacity: '1'}}>
-					<div className='question-section'>
-						<Typography className='question-count' component="div">
-							<span>Question {currentQuestion + 1}</span>/{questions.length}
-						</Typography>
-						<Typography className='question-text'>{questions[currentQuestion].questionText}
-						</Typography>
-					</div>
-					<Typography className='answer-section' component="div">
-						{questions[currentQuestion].answerOptions.map((answerOption) => (
-							<Button className='answers-button' onClick={() => handleAnswerClick(answerOption.value, categoryType)}>{answerOption.answerText}</Button>
-						))}
-					</Typography>
-				</div>
-		 </Typography>
-		);
+  if (isQuizDone.done) {
+    return <LoadingPage />;
+  }
+
+  return (
+    <Typography className="quiz-app" component="div">
+      <div style={{ opacity: "1" }}>
+        <div className="question-section">
+          <Typography className="question-count" component="div">
+            <span>Question {currentQuestion + 1}</span>/{questions.length}
+          </Typography>
+          <Typography className="question-text">
+            {questions[currentQuestion].questionText}
+          </Typography>
+        </div>
+        <Typography className="answer-section" component="div">
+          {questions[currentQuestion].answerOptions.map((answerOption) => (
+            <Button
+              className="answers-button"
+              onClick={() =>
+                handleAnswerClick(answerOption.value, categoryType)
+              }
+            >
+              {answerOption.answerText}
+            </Button>
+          ))}
+        </Typography>
+      </div>
+    </Typography>
+  );
 }
